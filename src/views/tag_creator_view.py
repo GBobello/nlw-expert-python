fc3a51c097ec9554068eb1baa1074e9ce2707d90
GBobello <bobello.gabriel@gmail.com>
2024-02-07 21:50:39 -0300

    feat: Creating the feature for barcodes

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,16 +1,19 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
         responsability for interacting with HTTP
     '''
     def validate_and_create(self, http_request: HttpRequest):
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body["product_code"]
 
         # lógica de regra de negócio
-        print('Estou na minha view')
-        print(product_code)
+        formatted_response = tag_creator_controller.create(product_code)
+
         # retorno http
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
