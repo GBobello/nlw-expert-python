995fd4e7849484ec9605e245fab0d9425eb3d76c
GBobello <bobello.gabriel@gmail.com>
2024-02-08 22:30:06 -0300

    test: Implementing unittests with pytest

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
+
 from src.errors.error_handler import handle_errors
+
+from src.validators.tag_creator_validator import tag_creator_validator
 
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
@@ -9,6 +12,7 @@ tags_routes_bp = Blueprint('tags_routes', __name__)
 def create_tags():
     response = None
     try:
+        tag_creator_validator(request)
         tag_creator_view = TagCreatorView()
 
         http_request = HttpRequest(body=request.json)
